204702c9952cca19f7223574b372dc742c86e6af
Matt Kocubinski <mkocubinski@gmail.com>
2019-01-10 10:24:06 -0500

    Version 1.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.purefn/starman "1.1.0-SNAPSHOT"
+(defproject org.purefn/starman "1.1.0"
   :description "A Redis implementation of the Bridges protocols."
   :url "https://github.com/PureFnOrg/starman"
   :license {:name "Eclipse Public License"
